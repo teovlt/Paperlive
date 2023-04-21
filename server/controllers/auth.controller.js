08c3aecd5fb8cc30823069f6f710baef05ed30b3
gabrielhalus <gabrielhalus@gmail.com>
2023-04-21 13:58:03 +0200

    add: updateTeam

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,66 @@
+const Team = require('../models/team.model');
+const jwt = require('jsonwebtoken');
+
+const maxAge = 24 * 60 * 60 * 1000;
+
+/**
+ * Create a JSON Web Token (JWT) using the given payload and a secret key
+ * @param {Object} payload - The data to be inclued in the JWT
+ * @returns {string} - The generated JWT.
+ */
+const createToken = (payload) => {
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: `${maxAge}ms` });
+};
+
+/**
+ * Handle signin up a new team
+ * @route POST /api/auth/register
+ * @group Teams
+ * @access Public
+ */
+module.exports.signUp = async (req, res) => {
+  try {
+    const { name, password } = req.body; // Extract the name and the password from the request
+    const team = await new Team({ name, password }).save(); //
+    const token = createToken({ id: team._id }); // Generate a JSON Web Token (JWT) using the team ID
+    // Set a cookie containing the JWT, with a maxAge (specified above) and set the httpOnly and sameSite for added security
+    res.cookie('jwt', token, { maxAge: maxAge, httpOnly: true, sameSite: 'Lax' });
+    return res.status(200).json({ message: 'Signed up successfully' }); // Send a success response with a message
+  } catch (error) {
+    // Error handling
+    if (error.code === 11000) error.message = 'team validation failed: name: already used';
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+/**
+ * Handle signing in a team member.
+ * @route /api/login
+ * @access Public
+ */
+module.exports.signIn = async (req, res) => {
+  try {
+    const { name, password } = req.body; // Extract the name and password from the request
+    const team = await Team.login(name, password); // Call the Team.login method to authenticate the team
+    const token = createToken({ id: team._id }); // Generate a JSON Web Token (JWT) using the team ID
+    // Set a cookie containing the JWT, with a maxAge (specified above) and set the httpOnly and sameSite for added security
+    res.cookie('jwt', token, { maxAge: maxAge, httpOnly: true, sameSite: 'Lax' });
+    return res.status(200).json({ message: 'Signed in successfully' }); // Send a success response with a message
+  } catch (error) {
+    // Error handling
+    return res.status(200).json(error.message);
+  }
+};
+
+module.exports.signOut = async (req, res) => {
+  try {
+    // Clear the JWT cookie
+    res.clearCookie('jwt');
+
+    // Send a success response
+    res.status(200).json({ message: 'Signed out successfully' });
+  } catch (error) {
+    // Error handling
+    return res.status(500).json(error.message);
+  }
+};
--- server/routes/team.routes.js
+++ server/routes/team.routes.js
@@ -3,8 +3,8 @@ const { verifyToken } = require('../middlewares/auth.middleware');
 const Router = require('express').Router();
 
 Router.get('/', teamController.listTeams);
-Router.get('/:teamId', verifyToken, teamController.readTeam);
-Router.patch('/:teamId', verifyToken, teamController.updateTeam);
+Router.get('/:teamId', teamController.readTeam);
+Router.put('/:teamId', verifyToken, teamController.updateTeam);
 Router.delete('/:teamId', verifyToken, teamController.deleteTeam);
 
 module.exports = Router;
