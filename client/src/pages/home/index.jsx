a5f22abc0ee96c97f65f5ec4fe769583e5e6660a
gabrielhalus <gabrielhalus@gmail.com>
2023-04-27 09:23:28 +0200

    fix: merge

--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useLogout from '../../hooks/useLogout';
 import useAuth from '../../hooks/useAuth';
-import { Main, NavPage, SideBar, GridWrapper, Button, Link } from '../../theme/appElements';
-import { TableContribution, Container, MainText, MainTab } from './homeElements';
+import {
+  Main,
+  NavPage,
+  SideBar,
+  GridWrapper,
+  Button,
+  Link,
+  Heading2,
+  Caption,
+  Select,
+  DivTop,
+  LinkIcon,
+  Heading1,
+} from '../../theme/appElements';
+import {
+  TableContribution,
+  Container,
+  MainText,
+  MainTab,
+  DivCheck,
+  DivBtnsEdit,
+  BtnCancel,
+  BtnSave,
+  Img,
+  Form,
+} from './homeElements';
 import CardContribution from '../../components/cardSoumission';
 import NavBar from '../../components/navbar';
+import Input from '../../components/Input';
+import Popup from '../../components/popup';
+import {
+  UilChart,
+  UilBook,
+  UilNewspaper,
+  UilLinkAlt,
+  UilLocationPoint,
+  UilBooks,
+} from '@iconscout/react-unicons';
 
 const Home = () => {
   const navigate = useNavigate();
@@ -17,49 +51,152 @@ const Home = () => {
     navigate('/');
   };
 
+  const [showEditAccount, setshowEditAccount] = useState(true);
+  const [showPopup, setShowPopup] = useState(false);
+  const [teamName, setTeamName] = useState('Mon équipe');
+  const [teamDesc, setteamDesc] = useState('Notre description');
+  const [teamLoc, setTeamLoc] = useState('Ma ville');
+  const [teamWebSite, setTeamWebSite] = useState('Mon website');
+
+  const handleEditAccount = () => {
+    setshowEditAccount(!showEditAccount);
+  };
+
+  let dernierBoutonClique = null;
+
+  function handleClick(buttonId) {
+    dernierBoutonClique = buttonId;
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (dernierBoutonClique === 'btnCancel') {
+      //nombreux problemes
+      if (teamName != e.target.name.value) {
+        console.log('ta changé le nom !!!!');
+        setShowPopup(true);
+        handleEditAccount();
+      } else {
+        handleEditAccount();
+
+        console.log('tu na rien changé du tt');
+      }
+
+      //verif modif ou non
+      /*
+      if (e.target.name.value == '') {
+      } else {
+      }
+      */
+    } else if (dernierBoutonClique === 'btnSave') {
+      handleSave(e);
+      handleEditAccount();
+    }
+  }
+
+  function handleSave(e) {
+    console.log('Changements sauvegardés !');
+    setTeamName(() => e.target.name.value);
+    setteamDesc(() => e.target.about.value);
+    setTeamLoc(() => e.target.location.value);
+    setTeamWebSite(() => e.target.webSite.value);
+
+    //sauvegarde
+
+    setShowPopup(false);
+    handleEditAccount();
+  }
+
+  function handleCancel() {
+    console.log('Changements NON sauvegardés !');
+    //Ne fait rien
+    setShowPopup(false);
+  }
+
   return (
     <>
       <Button onClick={signOut}>Sign Out : {name}</Button>
       <NavBar />
+      <DivTop></DivTop>
+      {showPopup && (
+        <Popup
+          title='Unsaved changements !'
+          desc='Watch out, you got some unsaved changements. Do you want to save them ?'
+          confirm={handleSave}
+          cancel={handleCancel}
+        />
+      )}
       <Container>
         <GridWrapper>
           <NavPage>
-            <Link>overview</Link>
-            <Link>contributions</Link>
-            <Link>statistics</Link>
+            <LinkIcon>
+              <UilBook /> Overview
+            </LinkIcon>
+            <LinkIcon>
+              <UilNewspaper /> Contributions
+            </LinkIcon>
+            <LinkIcon>
+              <UilChart /> Statistics
+            </LinkIcon>
           </NavPage>
 
-          <SideBar>
-            <img src='/vite.svg' alt='' />
-            <h2>Erods</h2>
-            <h4>Computer sciences</h4>
-            <Button>Edit team profil</Button>
-            <span>
-              <p>icon</p>11 members
-            </span>
-            <span>
-              <p>icon</p>8 contributions
-            </span>
-            <br />
-            <span>
-              <p>icon</p>grenoble,france
-            </span>
-            <span>
-              <p>icon</p>
-              <Link to='https://www.amazon.com'>www.amazon.com</Link>
-            </span>
-          </SideBar>
+          {showEditAccount ? (
+            <SideBar>
+              <Img src='/userGroupe.jpg' alt='' />
+              <div>
+                <Heading1>{teamName}</Heading1>
+                <Caption>Computer sciences</Caption>
+              </div>
+              <Button onClick={handleEditAccount}>Edit team profil</Button>
+              <span>
+                <UilBooks />8 contributions
+              </span>
+              <span>
+                <UilLocationPoint /> {teamLoc}
+              </span>
+              <span>
+                <UilLinkAlt />
+                <Link to='https://www.amazon.com'>{teamWebSite}</Link>
+              </span>
+            </SideBar>
+          ) : (
+            <SideBar>
+              <Img src='/userGroupe.jpg' alt='' />
+              <Form onSubmit={handleSubmit}>
+                <DivCheck>
+                  <p>Visibility:</p>
+                  <Select>
+                    <option value='option1'>Private</option>
+                    <option value='option2'>Public</option>
+                  </Select>
+                </DivCheck>
+
+                <Input label='Name of the team' id='name' type='text' defaultValue={teamName} />
+
+                <Input label='About us' id='about' type='text' defaultValue={teamDesc} />
+
+                <Input label='Location' id='location' type='text' defaultValue={teamLoc} />
+
+                <Input label='WebSite' id='webSite' type='text' defaultValue={teamWebSite} />
+
+                <DivBtnsEdit>
+                  <BtnCancel onClick={() => handleClick('btnCancel')} id='btnCancel' type='submit'>
+                    Cancel
+                  </BtnCancel>
+                  <BtnSave onClick={() => handleClick('btnSave')} id='btnSave' type='submit'>
+                    Save
+                  </BtnSave>
+                </DivBtnsEdit>
+              </Form>
+            </SideBar>
+          )}
           <Main>
             <MainText>
-              <h2>ABOUT US</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim vero tempore
-                reprehenderit quibusdam. Totam maiores expedita ea ut et tempore necessitatibus,
-                voluptas, aspernatur tempora autem consectetur placeat iste explicabo similique?
-              </p>
+              <Heading2>About us</Heading2>
+              <p>{teamDesc}</p>
             </MainText>
             <MainTab>
-              <h2>RECENT ACTIVITY</h2>
+              <Heading2>Recent activity</Heading2>
               <TableContribution>
                 <CardContribution />
                 <CardContribution />
@@ -75,32 +212,3 @@ const Home = () => {
 };
 
 export default Home;
-
-// a mettre dans main si editProfil
-/*
-
-                  <DivCheck>
-              <h2>PUBLIC PROFIL </h2>
-              <input type='checkbox' name='public' />
-            </DivCheck>
-            <DivInfos>
-              <h2>NAME OF THE TEAM</h2>
-              <Input placeholder={'ERODS'} type={'text'} />
-            </DivInfos>
-            <DivInfos>
-              <h2>ABOUT US</h2>
-              <Input placeholder={'WE ARE THE BEST TEAM OF THE LIG'} type={'text'} />
-            </DivInfos>
-            <DivInfos>
-              <h2>LOCATION</h2>
-              <Input placeholder={'PARIS'} type={'text'} />
-            </DivInfos>
-            <DivInfos>
-              <h2>WEBSITE URL</h2>
-              <Input placeholder={'www.teamWeb.com'} type={'text'} />
-            </DivInfos>
-            <DivBtns>
-              <Button>Cancel</Button>
-              <Button>Save</Button>
-            </DivBtns>
-*/
