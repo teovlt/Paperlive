d529148671f4ab16b2eba6128e006484c725ad97
teo_vlt <teo.villet@etu.univ-grenoble-alpes.fr>
2023-05-02 16:48:34 +0200

    tests unitaires teamController 100%

--- server/tests/teamController.test.js
+++ server/tests/teamController.test.js
@@ -16,100 +16,155 @@ afterAll(async () => {
   await mongoose.disconnect();
 });
 
-// describe('GET /api/teams', () => {
-//   const testData = [
-//     new Team({ name: 'Team1', password: 'password1' }),
-//     new Team({ name: 'Team2', password: 'password2' }),
-//     new Team({ name: 'Team3', password: 'password3' }),
-//   ];
+describe('GET /api/teams', () => {
+  const testData = [
+    new Team({ name: 'Team1', password: 'password1' }),
+    new Team({ name: 'Team2', password: 'password2' }),
+    new Team({ name: 'Team3', password: 'password3' }),
+  ];
 
-//   beforeAll(async () => {
-//     // insert the test data into the database before running the tests
-//     await Team.insertMany(testData);
-//   });
+  beforeAll(async () => {
+    // insert the test data into the database before running the tests
+    await Team.insertMany(testData);
+  });
 
-//   afterAll(async () => {
-//     // remove test data from the database after running the tests
-//     testData.forEach(async (team) => {
-//       await Team.deleteOne({ _id: team._id });
-//     });
-//   });
+  afterAll(async () => {
+    // remove test data from the database after running the tests
+    testData.forEach(async (team) => {
+      await Team.deleteOne({ _id: team._id });
+    });
+  });
 
-//   it('should return a 200 OK response with a list of teams excluding the password field', async () => {
-//     const res = await request(app).get('/api/teams');
+  it('should return a 200 OK response with a list of teams', async () => {
+    const res = await request(app)
+      .get('/api/teams')
+      .set('Authorization', `Bearer ${generateAccessToken('123')}`);
+    expect(res.status).toBe(200);
+  });
 
-//     expect(res.status).toBe(200);
-//     //expect(res.body.length).toBe(3);
-//     res.body.forEach((team) => {
-//       expect(team.password).toBeUndefined();
-//     });
-//   });
+  it('should return a 500 error with message when an error occurs', async () => {
+    // Mock the Team.find() method to throw an error
+    jest.spyOn(Team, 'find').mockImplementationOnce(() => {
+      throw new Error('Test error');
+    });
 
-//   it('should handle errors properly', async () => {
-//     // mock the team.find() method to throw an error
-//     const error = new Error('Test error');
-//     jest.spyOn(Team, 'find').mockImplementationOnce(() => {
-//       throw error;
-//     });
+    // Send a GET request to the endpoint
+    const response = await request(app)
+      .get('/api/teams')
+      .set('Authorization', `Bearer ${generateAccessToken('123')}`);
+    // Assert that the response has a 500 status code and contains an error message
+    expect(response.status).toBe(500);
+    expect(response.body.message).toBeDefined();
+  });
+});
 
-//     const res = await request(app).get('/api/teams');
+describe('GET /api/teams/:teamId', () => {
+  let team;
 
-//     expect(res.status).toBe(500);
-//     expect(res.body).toEqual({ message: error.message });
-//   });
-// });
+  beforeEach(async () => {
+    // Create a team for testing
+    team = await Team.create({ name: 'Test Team', password: 'password' });
+  });
 
-// describe('GET /api/teams/:teamId', () => {
-//   let team;
+  afterEach(async () => {
+    // Delete the test team from the database
+    await Team.deleteOne({ _id: team._id });
+  });
 
-//   beforeEach(async () => {
-//     // Create a team for testing
-//     team = await Team.create({ name: 'Test Team', password: 'password' });
-//   });
+  it('should return a team object with password field excluded', async () => {
+    const res = await request(app)
+      .get(`/api/teams/${team._id}`)
+      .set('Authorization', `Bearer ${generateAccessToken('123')}`);
 
-//   afterEach(async () => {
-//     // Delete the test team from the database
-//     await Team.deleteOne({ _id: team._id });
-//   });
+    const { password, ...teamWithoutPassword } = { ...team._doc, _id: team._doc._id.toString() };
 
-//   it('should return a team object with password field excluded', async () => {
-//     const res = await request(app).get(`/api/teams/${team._id}`);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual(teamWithoutPassword);
+  });
 
-//     const { password, ...teamWithoutPassword } = { ...team._doc, _id: team._doc._id.toString() };
+  it('should return a 404 Not Found when the team is not found', async () => {
+    const invalidId = new mongoose.Types.ObjectId();
+    const res = await request(app)
+      .get(`/api/teams/${invalidId}`)
+      .set('Authorization', `Bearer ${generateAccessToken('123')}`);
 
-//     expect(res.status).toBe(200);
-//     expect(res.body).toEqual(teamWithoutPassword);
-//   });
+    expect(res.status).toBe(404);
+    expect(res.body).toEqual({ message: 'Team not found' });
+  });
 
-//   it('should return a 404 Not Found when the team is not found', async () => {
-//     const invalidId = new mongoose.Types.ObjectId();
-//     const res = await request(app).get(`/api/teams/${invalidId}`);
+  it('should return a 500 Internal Server Error response with an error message if an invalid team ID is provided', async () => {
+    const invalidId = 'invalidId';
+    const res = await request(app)
+      .get(`/api/teams/${invalidId}`)
+      .set('Authorization', `Bearer ${generateAccessToken('123')}`);
 
-//     expect(res.status).toBe(404);
-//     expect(res.body).toEqual({ message: 'Team not found' });
-//   });
+    expect(res.status).toBe(500);
+    expect(res.body).toEqual({ message: `Invalid ID: ${invalidId}` });
+  });
 
-//   it('should return a 500 Internal Server Error response with an error message if an invalid team ID is provided', async () => {
-//     const invalidId = 'invalidId';
-//     const res = await request(app).get(`/api/teams/${invalidId}`);
+  it('should handle errors properly', async () => {
+    // mock the team.findOne() method to throw an error
+    const error = new Error('Test error');
+    jest.spyOn(Team, 'findOne').mockImplementationOnce(() => {
+      throw error;
+    });
 
-//     expect(res.status).toBe(500);
-//     expect(res.body).toEqual({ message: `Invalid ID: ${invalidId}` });
-//   });
+    const res = await request(app)
+      .get(`/api/teams/${team._id}`)
+      .set('Authorization', `Bearer ${generateAccessToken('123')}`);
 
-//   it('should handle errors properly', async () => {
-//     // mock the team.findOne() method to throw an error
-//     const error = new Error('Test error');
-//     jest.spyOn(Team, 'findOne').mockImplementationOnce(() => {
-//       throw error;
-//     });
+    expect(res.status).toBe(500);
+    expect(res.body).toEqual({ message: error.message });
+  });
+});
 
-//     const res = await request(app).get(`/api/teams/${team._id}`);
+describe('GET /api/teams/me', () => {
+  let team;
 
-//     expect(res.status).toBe(500);
-//     expect(res.body).toEqual({ message: error.message });
-//   });
-// });
+  beforeEach(async () => {
+    // Create a team for testing
+    team = await Team.create({ name: 'Test Team', password: 'password' });
+  });
+
+  afterEach(async () => {
+    // Delete the test team from the database
+    await Team.deleteOne({ _id: team._id });
+  });
+
+  it('should return 200 OK response with the team object', async () => {
+    const res = await request(app)
+      .get(`/api/teams/me`)
+      .set('Authorization', `Bearer ${generateAccessToken(team._id)}`);
+
+    expect(res.status).toBe(200);
+    expect(res.body._id).toEqual(team._id.toString());
+  });
+
+  it('should return 404 Not Found response with an error message', async () => {
+    const unknownId = new mongoose.Types.ObjectId();
+
+    const res = await request(app)
+      .get(`/api/teams/me`)
+      .set('Authorization', `Bearer ${generateAccessToken(unknownId)}`);
+
+    expect(res.status).toBe(404);
+    expect(res.body).toEqual({ error: 'Team not found' });
+  });
+
+  it('handle errors properly', async () => {
+    const error = new Error('Test error');
+    jest.spyOn(Team, 'findOne').mockImplementationOnce(() => {
+      throw error;
+    });
+
+    const res = await request(app)
+      .get(`/api/teams/me`)
+      .set('Authorization', `Bearer ${generateAccessToken(team._id)}`);
+
+    expect(res.status).toBe(500);
+    expect(res.body).toEqual({ error: error.message });
+  });
+});
 
 describe('PUT /api/teams', () => {
   let team;
