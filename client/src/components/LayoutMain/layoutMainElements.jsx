8156b8c5c27073697698114d5cabd7ca0dfbf21e
teo_vlt <teo.villet@etu.univ-grenoble-alpes.fr>
2023-05-03 13:36:31 +0200

    add : component LayoutMain

---
+++ client/src/components/LayoutMain/layoutMainElements.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  grid-area: 2 / 2 / 4 / 3;
+  margin-left: 72px;
+  padding-block: 32px;
+  display: flex;
+  flex-direction: column;
+  row-gap: 24px;
+`;
