d561a8d4cd7b72685612baaab17da829560a7ef6
gabrielhalus <gabrielhalus@gmail.com>
2023-05-04 12:54:00 +0200

    add: file upload method for contributions abstract

--- server/src/controllers/uploadController.js
+++ server/src/controllers/uploadController.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const Team = require('../models/teamModel');
+const Contribution = require('../models/contributionModel');
 
 const profileStorage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -13,11 +14,19 @@ const profileStorage = multer.diskStorage({
   },
 });
 
+const contributionStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/contribution/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, req.params.contributionId + '.pdf');
+  },
+});
+
 module.exports.getProfile = (req, res) => {
   const filePath = path.join(__dirname, '../../uploads/profile/', req.params.fileName);
   fs.exists(filePath, (exists) => {
-    if (exists) 
-    res.sendFile(filePath);
+    if (exists) res.sendFile(filePath);
     else return res.status(404).json({ error: 'File not found' });
   });
 };
@@ -26,6 +35,7 @@ module.exports.uploadProfile = (req, res) => {
   const upload = multer({ storage: profileStorage });
   upload.single('file')(req, res, async (err) => {
     if (err) return res.status(400).json({ message: 'Error uploading file', error: err });
+
     const result = await Team.updateOne(
       { _id: req.teamId },
       { $set: { picture: req.file?.filename } }
@@ -37,5 +47,25 @@ module.exports.uploadProfile = (req, res) => {
         .status(200)
         .json({ message: 'File uploaded successfully', filename: req.file.filename });
     }
+  });
+};
+
+module.exports.uploadContribution = (req, res) => {
+  const upload = multer({ storage: contributionStorage });
+  upload.single('file')(req, res, async (err) => {
+    if (err) return res.status(400).json({ message: 'Error uploading file', error: err });
+
+    const result = await Contribution.updateOne(
+      { _id: req.params.contributionId },
+      { $set: { abstract: req.file?.filename } }
+    );
+
+    // If the update was successful, return a 200 OK response with a success message
+    if (result.matchedCount > 0) {
+      return res
+        .status(200)
+        .json({ message: 'File uploaded successfully', filename: req.file?.filename });
+    }
+    return res.status(404).json({ error: 'Contribution not found' });
   });
 };
--- server/src/routes/uploadRoutes.js
+++ server/src/routes/uploadRoutes.js
@@ -3,6 +3,12 @@ const uploadController = require('../controllers/uploadController');
 const { authenticateAccessToken } = require('../middlewares/authenticationMiddleware');
 
 Router.get('/profile/:fileName', uploadController.getProfile);
+
 Router.post('/profile', authenticateAccessToken, uploadController.uploadProfile);
+Router.post(
+  '/contribution/:contributionId',
+  authenticateAccessToken,
+  uploadController.uploadContribution
+);
 
 module.exports = Router;
