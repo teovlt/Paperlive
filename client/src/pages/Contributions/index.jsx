3a7dcfc6db3de10af9b3471b83fd9a7c47c38543
teo_vlt <teo.villet@etu.univ-grenoble-alpes.fr>
2023-05-05 16:32:32 +0200

    add: initialisation page contribution

--- client/src/pages/Contributions/index.jsx
+++ client/src/pages/Contributions/index.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import useAuth from '../../hooks/useAuth';
+import { HiPlus } from 'react-icons/hi2';
+import { SectionMain, Btn } from './contributionsElements';
 
 function Contributions() {
-  return <div>Contributions</div>;
+  const { auth, setAuth } = useAuth();
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <h2>{auth.contributions?.length} contribution(s)</h2>
+      <SectionMain>
+        <input type='text' placeholder='Rechercher une contribution' />
+        <Btn onClick={() => navigate('/contributions/new')}>
+          Add a contribution
+          <HiPlus />
+        </Btn>
+      </SectionMain>
+    </>
+  );
 }
 
 export default Contributions;
