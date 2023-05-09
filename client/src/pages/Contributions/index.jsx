d3051eb312f9390583e20e6e4bb76737fb0050a5
teo_vlt <teo.villet@etu.univ-grenoble-alpes.fr>
2023-05-09 14:52:14 +0200

    add : table of contributions

--- client/src/pages/Contributions/index.jsx
+++ client/src/pages/Contributions/index.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
-import { HiPlus } from 'react-icons/hi2';
-import { SectionMain, Btn } from './contributionsElements';
+import { HiPlus, HiChevronDown, HiOutlineClock } from 'react-icons/hi2';
+import { SectionMain, Btn, Table } from './contributionsElements';
 import { useTranslation } from 'react-i18next';
 
 function Contributions() {
   const { t } = useTranslation();
-  const { auth, setAuth } = useAuth();
+  const { auth } = useAuth();
   const navigate = useNavigate();
 
   return (
@@ -18,8 +18,48 @@ function Contributions() {
         <Btn onClick={() => navigate('/contributions/new')}>
           {t('contribution.addContribution')}
           <HiPlus />
-        </Btn>
+        </Btn>{' '}
       </SectionMain>
+      <Table>
+        <thead>
+          <tr>
+            <th className='title'>
+              Title of the contribution
+              <HiChevronDown />
+            </th>
+            <th>
+              Date
+              <HiChevronDown />
+            </th>
+            <th>
+              Role
+              <HiChevronDown />
+            </th>
+            <th>
+              Etat <HiChevronDown />
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {!auth.contributions?.length > 0 ? (
+            <tr className='trNoContri'>
+              <td>Vous ne possédez pas de contributions, rajoutez en une !</td>
+            </tr>
+          ) : (
+            auth.contributions.map((contribution) => (
+              <tr className='trBody'>
+                <td className='title'>{contribution.title}</td>
+                <td className='date'>{contribution.startDate}</td>
+                <td className='role'>{contribution.role}</td>
+                <td className='etat'>
+                  {contribution.state}
+                  <HiOutlineClock />
+                </td>
+              </tr>
+            ))
+          )}
+        </tbody>
+      </Table>
     </>
   );
 }
