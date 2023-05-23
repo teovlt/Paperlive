70fe64f9fe36220768f42dd4bd61d02f44b7b02e
gabrielhalus <gabrielhalus@gmail.com>
2023-05-23 09:28:46 +0200

    add: Change password

--- client/src/pages/Settings/security.jsx
+++ client/src/pages/Settings/security.jsx
@@ -1,24 +1,65 @@
 import React, { useState, useEffect } from 'react';
-import { Heading2 } from '../../theme/appElements';
+import { Button, Heading2 } from '../../theme/appElements';
 import Input from '../../components/Input';
+import Chips from '../../components/Chips';
 import { useTranslation } from 'react-i18next';
-import useAuth from '../../hooks/useAuth';
 import useAxiosPrivate from '../../hooks/useAxiosPrivate';
+import { useConfirm } from '../../context/ConfirmContext';
 
 const SecuritySettings = () => {
-  const { t } = useTranslation();
-  const { auth, setAuth } = useAuth();
-
+  const { t, i18n } = useTranslation();
+  const { confirm } = useConfirm();
   const axiosPrivate = useAxiosPrivate();
 
-  const [profilData, setProfilData] = useState(auth);
+  const [oldPassword, setOldPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [newPasswordConf, setNewPasswordConf] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+
+  const passwordRegex =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?/~\|])(?!.*\s).{8,}$/;
 
   useEffect(() => {
-    setProfilData(auth);
-  }, [auth]);
+    setErrMsg('');
+  }, [oldPassword, newPassword, newPasswordConf]);
 
-  const verifyPassword = () => {
-    console.log(e.target.value);
+  useEffect(() => {
+    if (newPasswordConf && newPassword !== newPasswordConf) {
+      setErrMsg(t('register.errorPasswordConf'));
+    }
+    if (newPassword && !passwordRegex.test(newPassword)) {
+      setErrMsg(t('register.errorPasswordFormat'));
+    }
+  }, [newPassword, newPasswordConf, i18n.resolvedLanguage]);
+
+  const handleSubmit = async () => {
+    if (!errMsg) {
+      const confirmed = await confirm({
+        title: t('settings.changePasswordConfirmationTitle'),
+        caption: t('settings.changePasswordConfirmationCaption'),
+        cancelLabel: t('global.cancel'),
+        confirmLabel: t('global.confirm'),
+      });
+
+      if (confirmed) {
+        try {
+          const response = await axiosPrivate.put('/teams/change-password', {
+            newPassword: newPassword,
+            oldPassword: oldPassword,
+          });
+          setOldPassword('');
+          setNewPassword('');
+          setNewPasswordConf('');
+          // TODO: Notification succes
+        } catch (error) {
+          if (!error?.response) {
+            setErrMsg(t('authentication.servorError'));
+          } else {
+            setErrMsg(t('settings.passwordError'));
+          }
+        }
+      }
+    }
   };
 
   return (
@@ -26,6 +67,37 @@ const SecuritySettings = () => {
       <Heading2 style={{ borderBottom: '1px solid var(--black-quaternary)' }}>
         Password and authentification
       </Heading2>
+      <Input
+        id='oldPassword'
+        type='password'
+        label={t('settings.oldPassword')}
+        autoComplete='off'
+        small
+        value={oldPassword}
+        onChange={(e) => setOldPassword(e.target.value)}
+      />
+      <Input
+        id='newPassword'
+        type='password'
+        label={t('settings.newPassword')}
+        autoComplete='off'
+        small
+        value={newPassword}
+        onChange={(e) => setNewPassword(e.target.value)}
+      />
+      <Input
+        id='newPasswordConf'
+        type='password'
+        label={t('settings.newPasswordConf')}
+        autoComplete='off'
+        small
+        value={newPasswordConf}
+        onChange={(e) => setNewPasswordConf(e.target.value)}
+      />
+      {errMsg && <Chips type='negative'>{errMsg}</Chips>}
+      <Button secondary onClick={handleSubmit}>
+        Change password
+      </Button>
     </>
   );
 };
