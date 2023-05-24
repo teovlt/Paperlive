3525db499f2099ca5ba17688deba02bc0417b0fc
VILLET Teo <teo.villet@etu.univ-grenoble-alpes.fr>
2023-05-24 11:33:36 +0200

    add: editSection in FormStep5 newSubmission

--- client/src/pages/Submission/NewSubmission/FormStep5.jsx
+++ client/src/pages/Submission/NewSubmission/FormStep5.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import { Button} from '../../../theme/appElements';
-import { LinearContainer } from '../../Contributions/contributionsElements';
+import { Button, Heading3, Label } from '../../../theme/appElements';
+import { LinearContainer, Link, MainWrapper } from '../../Contributions/contributionsElements';
 
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
@@ -21,6 +21,61 @@ const FormStep5 = ({ submissionData, previous, goTo }) => {
 
   return (
     <>
+      <MainWrapper>
+        <Heading3>Informations</Heading3>
+        <Label>
+          {t('submission.title')}:<span>{submissionData.title}</span>
+        </Label>
+        <Label>
+          Date:
+          <span>
+            {new Intl.DateTimeFormat(i18n.language, {
+              day: '2-digit',
+              month: '2-digit',
+              year: 'numeric',
+            }).format(new Date(submissionData?.submissionDate))}
+          </span>
+        </Label>
+        <Label>
+          type:<span>{submissionData?.type}</span>
+        </Label>
+        <Label>
+          state:<span>{submissionData?.state}</span>
+        </Label>
+        <Label>la contribution en lien</Label>
+        <Link onClick={() => goTo(0)}> {t('contribution.editForm')}</Link>
+      </MainWrapper>
+      <MainWrapper>
+        <Heading3>{t('contribution.files')}</Heading3>
+        <Label>
+          Abstract:<span>{submissionData?.abstract}</span>
+        </Label>
+        <Label>
+          zipFolder:<span>{submissionData?.zipFolder}</span>
+        </Label>
+        <Label>
+          compiledPDF:<span>{submissionData?.compiledPDF}</span>
+        </Label>
+        <Label>
+          diffPDF:<span>{submissionData?.diffPDF}</span>
+        </Label>
+        <Link onClick={() => goTo(1)}>{t('contribution.editForm')}</Link>
+      </MainWrapper>
+      <MainWrapper>
+        <Heading3>Authors</Heading3>
+        <Label>
+          Authors:<span>{submissionData?.authors}</span>
+        </Label>
+        <Link onClick={() => goTo(2)}>{t('contribution.editForm')}</Link>
+      </MainWrapper>
+      <MainWrapper>
+        <Heading3>Venue</Heading3>
+        <Label>
+          Venue:<span>{submissionData?.venue}</span>
+        </Label>
+        <Link onClick={() => goTo(3)}>{t('contribution.editForm')}</Link>
+      </MainWrapper>
+
       <LinearContainer>
         <Button style={{ width: '160px' }} type='neutral' onClick={() => previous()}>
           {t('global.previous')}
