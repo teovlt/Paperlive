b7c771f90cced319e458c900ce2a8708de0cd918
VILLET Teo <teo.villet@etu.univ-grenoble-alpes.fr>
2023-05-24 13:36:10 +0200

    improve: formStep with files

--- client/src/pages/Submission/NewSubmission/FormStep2.jsx
+++ client/src/pages/Submission/NewSubmission/FormStep2.jsx
@@ -4,50 +4,57 @@ import { Button, Heading3, Label } from '../../../theme/appElements';
 import { useTranslation } from 'react-i18next';
 import { LinearContainer } from '../../Contributions/contributionsElements';
 
-const FormStep2 = ({
-  submissionData,
-  setSubmissionData,
-  next,
-  previous,
-}) => {
+const FormStep2 = ({ submissionData, setSubmissionData, next, previous }) => {
   const { t } = useTranslation();
 
   return (
     <>
-      <Heading3>Abstract</Heading3>
-      <Label>
-        {t('contribution.fileSupported')}: <span>pdf</span>
-      </Label>
+      <Heading3>
+        Abstract{' '}
+        <Label>
+          {t('contribution.fileSupported')}: <span>pdf</span>
+        </Label>
+      </Heading3>
+
       <FileInput
         name='abstract'
         file={submissionData.abstract}
         endpoint='files/submission/abstract'
         onChange={(file) => setSubmissionData((prev) => ({ ...prev, abstract: file?.name }))}
       />
-      <Heading3>zipFolder</Heading3>
-      <Label>
-        {t('contribution.fileSupported')}: <span>zip</span>
-      </Label>
+      <Heading3>
+        zipFolder
+        <Label>
+          {t('contribution.fileSupported')}: <span>zip</span>
+        </Label>
+      </Heading3>
+
       <FileInput
         name='zipFolder'
         file={submissionData.zipFolder}
         endpoint='files/submission/zipfolder'
         onChange={(file) => setSubmissionData((prev) => ({ ...prev, zipFolder: file?.name }))}
       />
-      <Heading3>compiledPDF</Heading3>
-      <Label>
-        {t('contribution.fileSupported')}: <span>pdf</span>
-      </Label>
+      <Heading3>
+        compiledPDF
+        <Label>
+          {t('contribution.fileSupported')}: <span>pdf</span>
+        </Label>
+      </Heading3>
+
       <FileInput
         name='compiledPDD'
         file={submissionData.compiledPDF}
         endpoint='files/submission/compiledpdf'
         onChange={(file) => setSubmissionData((prev) => ({ ...prev, compiledPDF: file?.name }))}
       />
-      <Heading3>diffPDF</Heading3>
-      <Label>
-        {t('contribution.fileSupported')}: <span>pdf</span>
-      </Label>
+      <Heading3>
+        diffPDF
+        <Label>
+          {t('contribution.fileSupported')}: <span>pdf</span>
+        </Label>
+      </Heading3>
+
       <FileInput
         name='diffPDF'
         file={submissionData.diffPDF}
