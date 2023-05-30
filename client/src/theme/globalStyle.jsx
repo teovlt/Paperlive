0eecac7708c3105f9dd8730358e72806e951c042
VILLET Teo <teo.villet@etu.univ-grenoble-alpes.fr>
2023-05-30 14:51:09 +0200

    add black colors in globalStyle

--- client/src/theme/globalStyle.jsx
+++ client/src/theme/globalStyle.jsx
@@ -1,11 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
+import React, { useContext, useState, useEffect } from 'react';
+import { AccentContext, ThemeContext } from '../App';
 
 const GlobalStyle = createGlobalStyle`
+
   :root {
     font-size: 62.5%;
 
     /* Variables */
-    --accent: ${(props) => `rgb(${props.theme.accentColor})`};
+   
+
+
+  //  @media (prefers-color-scheme: light) {
+       --accent: ${(props) => `rgb(${props.theme.accentColor})`};
     --accent-vibrant: ${(props) => `rgba(${props.theme.accentColor}, 0.15)`};
     --positive: #42b983;
     --positive-vibrant: #42b98333;
@@ -20,7 +27,25 @@ const GlobalStyle = createGlobalStyle`
     --black-quaternary: #2222221e; 
     
     --white: #ffffff;
-    --white-50: #ffffff80
+    --white-50: #ffffff80;
+  //  }
+
+    /* @media (prefers-color-scheme: dark) {
+      --accent: ${(props) => `rgb(${props.theme.accentColor})`};
+    --accent-vibrant: ${(props) => `rgba(${props.theme.accentColor}, 0.15)`};
+    --positive: #1e6c23;
+    --positive-vibrant: #1e6c2333;
+    --notice: #e66700;
+    --notice-vibrant: #e6670033;
+    --negative: #bdbeb9;
+    --negative-vibrant: #bdbeb933;
+    --black: #ffffff;
+    --black-secondary: rgba(255, 255, 255, 0.75);
+    --black-tertiary: #ffffff7f;
+    --black-quaternary: #ffffff1e;
+    --white: #222222;
+    --white-50: #22222280;
+  } */
   }
 
   *, ::before, ::after {
