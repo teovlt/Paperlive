c432b4628ee3dd7b079fd88aa2767f1bd5094930
gabrielhalus <gabrielhalus@gmail.com>
2023-06-05 14:05:14 +0200

    🐛 FIX: venue errors

--- server/src/controllers/venueController.js
+++ server/src/controllers/venueController.js
@@ -1,15 +1,5 @@
 const Venue = require('../models/venueModel');
-
-// module.exports.createOrUpdateVenue = async (venue) => {
-//   const { _id, ...venueData } = venue;
-//   if (_id) {
-//     const updatedVenue = await Venue.findOne({ _id: _id });
-//     await updatedVenue.updateOne({ $set: { ...venueData } });
-//     return updatedVenue;
-//   } else {
-//     return await Venue.create({ ...venueData });
-//   }
-// };
+const ObjectId = require('mongoose').Types.ObjectId;
 
 module.exports.getVenues = async (req, res) => {
   const venues = await Venue.find();
