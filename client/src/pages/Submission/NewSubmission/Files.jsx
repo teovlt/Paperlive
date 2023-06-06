6f99812a41b6330532ce35b458362f388aa4c73a
VILLET Teo <teo.villet@etu.univ-grenoble-alpes.fr>
2023-06-06 14:09:07 +0200

    add: trad files new Submission

--- client/src/pages/Submission/NewSubmission/Files.jsx
+++ client/src/pages/Submission/NewSubmission/Files.jsx
@@ -3,43 +3,45 @@ import { useNavigate } from 'react-router-dom';
 import { Group, SectionContainer } from '../submissionElements';
 import { Button, Heading2 } from '../../../theme/appElements';
 import FileInput from '../../../components/NewFileInput';
+import { useTranslation } from 'react-i18next';
 
 const Files = ({ data, setData }) => {
   const navigate = useNavigate();
+  const { t } = useTranslation();
 
   return (
     <SectionContainer>
-      <Heading2>Files</Heading2>
+      <Heading2>{t('submission.files')}</Heading2>
       <FileInput
-        name='abstract'
+        name={t('submission.abstract')}
         MIMEType='pdf'
         setData={(file) => setData((prev) => ({ ...prev, abstract: file }))}
       />
       <FileInput
-        name='zipfolder'
+        name={t('submission.zipFolder')}
         MIMEType='zip'
         setData={(file) => setData((prev) => ({ ...prev, zipFolder: file }))}
       />
       <FileInput
-        name='compiledpdf'
+        name={t('submission.compiledPDF')}
         MIMEType='pdf'
         setData={(file) => setData((prev) => ({ ...prev, compiledPdf: file }))}
       />
       <FileInput
-        name='diffpdf'
+        name={t('submission.diffPDF')}
         MIMEType='pdf'
         setData={(file) => setData((prev) => ({ ...prev, diffPdf: file }))}
       />
       <FileInput
-        name='commentpdf'
+        name={t('submission.commentsPDF')}
         MIMEType='pdf'
         setData={(file) => setData((prev) => ({ ...prev, commentsPdf: file }))}
       />
       <Group inline>
         <Button type='neutral' onClick={() => navigate('../venue')}>
-          Previous
+          {t('global.previous')}
         </Button>
-        <Button onClick={() => navigate('../summary')}>Next</Button>
+        <Button onClick={() => navigate('../summary')}> {t('global.next')}</Button>
       </Group>
     </SectionContainer>
   );
