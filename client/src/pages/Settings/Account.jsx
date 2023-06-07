f7f2e42650958d6bd241473393d7c92b4b497274
gabrielhalus <gabrielhalus@gmail.com>
2023-06-07 07:50:32 +0200

    🐛 FIX: account settings layout

--- client/src/pages/Settings/Account.jsx
+++ client/src/pages/Settings/Account.jsx
@@ -94,145 +94,147 @@ const AccountSettings = () => {
 
   return (
     <>
-      <Heading2 style={{ borderBottom: '1px solid var(--black-quaternary)' }}>
-        {t('settings.pages.myAccount')}
-      </Heading2>
-      {!deleteConfirmation ? (
-        <>
-          <DivConnected>
-            <DivLeftInfos>
-              <RadioGroup
-                name='visibility'
-                label={t('sideBar.visibility')}
-                template={{
-                  radios: [
-                    {
-                      label: t('sideBar.private'),
-                      value: false,
-                      defaultChecked: profilData.visibility === false,
-                    },
-                    {
-                      label: t('sideBar.public'),
-                      value: true,
-                      defaultChecked: profilData.visibility === true,
-                    },
-                  ],
-                }}
-                onChange={(event) => {
-                  setProfilData((prev) => ({
-                    ...prev,
-                    visibility: JSON.parse(event.target.value),
-                  }));
-                }}
-              />
-              <TextArea
-                id='description'
-                label={t('sideBar.description')}
-                maxLength='240'
-                autoComplete='off'
-                small
-                value={profilData.description}
-                onChange={(e) => {
-                  const newProfilData = { ...profilData, description: e.target.value };
-                  setProfilData(newProfilData);
-                }}
-              />
-            </DivLeftInfos>
-
-            <div style={{ width: '200px' }}>
-              <Avatar />
-            </div>
-          </DivConnected>
-          <Input
-            id='location'
-            label={t('sideBar.location')}
-            autoComplete='off'
-            small
-            value={profilData.location}
-            onChange={(e) => {
-              const newProfilData = { ...profilData, location: e.target.value };
-              setProfilData(newProfilData);
-            }}
-          />
-          <Input
-            id='website'
-            label={t('sideBar.webSite')}
-            autoComplete='off'
-            small
-            value={profilData.website}
-            onChange={(e) => {
-              const newProfilData = { ...profilData, website: e.target.value };
-              setProfilData(newProfilData);
-            }}
-          />
-          <Button type='neutral' onClick={handleSubmit} style={{ width: '100%' }}>
-            {t('settings.profile.updateProfile')}
-          </Button>
-          <SectionContainer>
-            <Heading2
-              style={{
-                borderBottom: '1px solid var(--black-quaternary)',
-                color: 'var(--negative)',
-              }}>
-              {t('settings.profile.deleteAccount')}
-            </Heading2>
-            <Caption>{t('settings.profile.deleteAccountWarning1')}</Caption>
-
-            <Button
-              type='negative'
-              style={{ width: '250px' }}
-              onClick={() => setDeleteConfirmation(true)}>
-              {t('settings.profile.deleteAccount')}
-            </Button>
-          </SectionContainer>
-        </>
-      ) : (
-        <>
-          <Chips type='notice'>{t('settings.profile.deleteAccountWarning2')}</Chips>
-          <Caption>{t('settings.profile.deleteAccountWarning3')}</Caption>
-
-          <Input
-            id='name'
-            label={t('settings.profile.teamName')}
-            autoComplete='off'
-            small
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <Input
-            id='password'
-            type='password'
-            label={t('global.password')}
-            autoComplete='off'
-            small
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-
-          <DivConfirmDelete>
-            <Caption>{t('settings.profile.verificationPrompt')}</Caption>
-            <Input
-              id='confirm'
-              label={t('settings.profile.inputConfirm')}
+      <SectionContainer>
+        <Heading2 style={{ borderBottom: '1px solid var(--black-quaternary)' }}>
+          {t('settings.pages.myAccount')}
+        </Heading2>
+        <DivConnected>
+          <DivLeftInfos>
+            <RadioGroup
+              name='visibility'
+              label={t('sideBar.visibility')}
+              template={{
+                radios: [
+                  {
+                    label: t('sideBar.private'),
+                    value: false,
+                    defaultChecked: profilData.visibility === false,
+                  },
+                  {
+                    label: t('sideBar.public'),
+                    value: true,
+                    defaultChecked: profilData.visibility === true,
+                  },
+                ],
+              }}
+              onChange={(event) => {
+                setProfilData((prev) => ({
+                  ...prev,
+                  visibility: JSON.parse(event.target.value),
+                }));
+              }}
+            />
+            <TextArea
+              id='description'
+              label={t('sideBar.description')}
+              maxLength='240'
               autoComplete='off'
               small
-              value={confirmation}
-              onChange={(e) => setConfirmation(e.target.value)}
+              value={profilData.description}
+              onChange={(e) => {
+                const newProfilData = { ...profilData, description: e.target.value };
+                setProfilData(newProfilData);
+              }}
             />
-          </DivConfirmDelete>
+          </DivLeftInfos>
 
-          {errMsg && <Chips type='negative'>{errMsg}</Chips>}
+          <div style={{ width: '200px' }}>
+            <Avatar />
+          </div>
+        </DivConnected>
+        <Input
+          id='location'
+          label={t('sideBar.location')}
+          autoComplete='off'
+          small
+          value={profilData.location}
+          onChange={(e) => {
+            const newProfilData = { ...profilData, location: e.target.value };
+            setProfilData(newProfilData);
+          }}
+        />
+        <Input
+          id='website'
+          label={t('sideBar.webSite')}
+          autoComplete='off'
+          small
+          value={profilData.website}
+          onChange={(e) => {
+            const newProfilData = { ...profilData, website: e.target.value };
+            setProfilData(newProfilData);
+          }}
+        />
+        <Button type='neutral' onClick={handleSubmit} style={{ width: '100%' }}>
+          {t('settings.profile.updateProfile')}
+        </Button>
+      </SectionContainer>
+      {/*  */}
+      {/*  */}
+      {/*  */}
+      <SectionContainer>
+        <Heading2
+          style={{
+            borderBottom: '1px solid var(--black-quaternary)',
+            color: 'var(--negative)',
+          }}>
+          {t('settings.profile.deleteAccount')}
+        </Heading2>
+        <Caption>{t('settings.profile.deleteAccountWarning1')}</Caption>
 
-          <DivDeleteAccountBtns>
-            <Button style={{ width: '250px' }} type='neutral' onClick={handleCancel}>
-              {t('global.cancel')}
-            </Button>
-            <Button type='negative' style={{ width: '250px' }} onClick={handleDeleteAccount}>
-              {t('settings.profile.deleteAccount')}
-            </Button>
-          </DivDeleteAccountBtns>
-        </>
-      )}
+        <Button
+          type='negative'
+          style={{ width: '250px' }}
+          onClick={() => setDeleteConfirmation(true)}>
+          {t('settings.profile.deleteAccount')}
+        </Button>
+        {deleteConfirmation && (
+          <>
+            <Chips type='notice'>{t('settings.profile.deleteAccountWarning2')}</Chips>
+            <Caption>{t('settings.profile.deleteAccountWarning3')}</Caption>
+
+            <Input
+              id='name'
+              label={t('settings.profile.teamName')}
+              autoComplete='off'
+              small
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <Input
+              id='password'
+              type='password'
+              label={t('global.password')}
+              autoComplete='off'
+              small
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <DivConfirmDelete>
+              <Caption>{t('settings.profile.verificationPrompt')}</Caption>
+              <Input
+                id='confirm'
+                label={t('settings.profile.inputConfirm')}
+                autoComplete='off'
+                small
+                value={confirmation}
+                onChange={(e) => setConfirmation(e.target.value)}
+              />
+            </DivConfirmDelete>
+
+            {errMsg && <Chips type='negative'>{errMsg}</Chips>}
+
+            <DivDeleteAccountBtns>
+              <Button style={{ width: '250px' }} type='neutral' onClick={handleCancel}>
+                {t('global.cancel')}
+              </Button>
+              <Button type='negative' style={{ width: '250px' }} onClick={handleDeleteAccount}>
+                {t('settings.profile.deleteAccount')}
+              </Button>
+            </DivDeleteAccountBtns>
+          </>
+        )}
+      </SectionContainer>
     </>
   );
 };
