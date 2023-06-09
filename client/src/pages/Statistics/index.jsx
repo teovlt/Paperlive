0fe4378544f11e7117982afde42acf510f958de0
VILLET Teo <teo.villet@etu.univ-grenoble-alpes.fr>
2023-06-09 09:28:04 +0200

    add: fix: unit test contributionController

--- client/src/pages/Statistics/index.jsx
+++ client/src/pages/Statistics/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import useAuth from '../../hooks/useAuth';
 
-import { Heading2, SectionContainer } from '../../theme/appElements';
+import { Heading2, SectionContainer, Paragraph } from '../../theme/appElements';
 
 const Statistics = () => {
   const { auth } = useAuth();
@@ -9,10 +9,29 @@ const Statistics = () => {
   const contributions = auth.contributions;
   const submissions = auth.contributions.reduce((acc, curr) => acc.concat(curr.submissions), []);
 
+  const venues = submissions.map((submission) => submission.venue);
+
+  const ranks = venues.map((venue) => venue.rank);
+  const rankA = ranks.filter((rank) => rank === 'a');
+  const rankB = ranks.filter((rank) => rank === 'b');
+  const rankC = ranks.filter((rank) => rank === 'c');
+
+  const roles = auth.contributions.reduce((acc, curr) => acc.concat(curr.teamRole), []);
+  const leader = roles.filter((role) => role === 'leader');
+  const coLeader = roles.filter((role) => role === 'coLeader');
+  const guest = roles.filter((role) => role === 'guest');
+
   return (
     <>
       <SectionContainer>
-        <Heading2>Statistics</Heading2>
+        <Heading2> {ranks}</Heading2>
+        <Paragraph>Il y a : {rankA.length} rang A utilisé</Paragraph>
+        <Paragraph> Il y a : {rankB.length}rang B utilisé</Paragraph>
+        <Paragraph>Il y a : {rankC.length}rang C utilisé</Paragraph>
+        <Heading2> {roles}</Heading2>
+        <Paragraph>Il y a : {leader.length} contribution ou je suis leader</Paragraph>
+        <Paragraph> Il y a : {coLeader.length} contribution ou je suis coLeader</Paragraph>
+        <Paragraph>Il y a : {guest.length} contribution ou je suis guest</Paragraph>
       </SectionContainer>
     </>
   );
