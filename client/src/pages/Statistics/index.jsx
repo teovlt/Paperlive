8db5a923a68a2bab8d54efe16f9b610cca9f8277
gabrielhalus <gabrielhalus@gmail.com>
2023-06-10 09:48:54 +0200

    add Statistics

--- client/src/pages/Statistics/index.jsx
+++ client/src/pages/Statistics/index.jsx
@@ -1,20 +1,114 @@
-import React, { useEffect, useState } from 'react';
-import useAuth from '../../hooks/useAuth';
+import React from 'react';
 
-import { Heading2, SectionContainer } from '../../theme/appElements';
+import useAuth from '../../hooks/useAuth';
+import { useNavigate } from 'react-router-dom';
+
+import { Caption, Heading2, SectionContainer } from '../../theme/appElements';
+import { Bar, BarChart, CartesianGrid, Label, Legend, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Statistics = () => {
   const { auth } = useAuth();
+  const navigate = useNavigate();
 
   const contributions = auth.contributions;
   const submissions = auth.contributions.reduce((acc, curr) => acc.concat(curr.submissions), []);
 
+  const data = Object.entries(
+    contributions
+      .filter((c) => c.state === 'approved')
+      .reduce((acc, c) => {
+        c.submissions
+          .filter((s) => s.type === 'longPaper' && s.state === 'approved')
+          .sort((a, b) => new Date(a.submissionDate) - new Date(b.submissionDate))
+          .slice(0, 1)
+          .flatMap((s) => {
+            if (s.venue) {
+              if (acc[s.venue.rank]) {
+                if (acc[s.venue.rank][c.teamRole]) acc[s.venue.rank][c.teamRole] += 1;
+                else acc[s.venue.rank] = { ...acc[s.venue.rank], [c.teamRole]: 1 };
+              } else {
+                acc[s.venue.rank] = { [c.teamRole]: 1 };
+              }
+            }
+          });
+        return acc;
+      }, {})
+  ).map(([rank, grades]) => ({ rank, ...grades }));
+
+  const filters = {
+    period: { startingDate: 2000, endingDate: 2025 },
+  };
+
+  const data2 = Object.entries(
+    contributions
+      .filter((c) => c.state === 'approved')
+      .reduce((acc, c) => {
+        c.submissions
+          .filter((s) => s.type === 'longPaper' && s.state === 'approved')
+          .sort((a, b) => new Date(a.submissionDate) - new Date(b.submissionDate))
+          .slice(0, 1)
+          .flatMap((s) => {
+            const { _id: id, title, startDate } = c;
+            const { submissionDate: endDate } = s;
+
+            const monthDiff =
+              (new Date(endDate).getFullYear() - new Date(startDate).getFullYear()) * 12 +
+              (new Date(endDate).getMonth() - new Date(startDate).getMonth());
+
+            acc[id] = { title, monthDiff };
+          });
+        return acc;
+      }, {})
+  )
+    .map(([id, data]) => ({ id, ...data }))
+    .sort((a, b) => b.monthDiff - a.monthDiff);
+
+  if (contributions.length <= 0 || submissions.length <= 0)
+    return <Caption>Missing data for statistics </Caption>;
+
   return (
-    <>
-      <SectionContainer>
-        <Heading2>Statistics</Heading2>
-      </SectionContainer>
-    </>
+    <SectionContainer>
+      <Heading2>Statistics</Heading2>
+      <BarChart
+        width={752}
+        height={500}
+        margin={{
+          top: 15,
+        }}
+        data={data}>
+        <CartesianGrid strokeDasharray='3 3' />
+        <XAxis dataKey='rank' tick={{ fontSize: 12 }} />
+        <YAxis interval={1} tick={{ fontSize: 12 }}>
+          <Label
+            value='Nombre de participations'
+            offset={20}
+            angle={-90}
+            fontSize={12}
+            textAnchor='middle'
+          />
+        </YAxis>
+        <Legend />
+        <Bar dataKey='leader' fill='#20a4f3' />
+        <Bar dataKey='coLeader' fill='#2ec4b6' />
+        <Bar dataKey='guest' fill='#ff3366' />
+      </BarChart>
+
+      <BarChart width={752} height={500} margin={{ top: 15 }} data={data2}>
+        <CartesianGrid strokeDasharray='3 3' />
+
+        <XAxis dataKey='title' tick={{ fontSize: 12 }} />
+
+        <YAxis dataKey='monthDiff' tick={{ fontSize: 12 }}>
+          <Label value='Durée (mois)' offset={20} angle={-90} fontSize={12} textAnchor='middle' />
+        </YAxis>
+
+        <Bar
+          dataKey='monthDiff'
+          fill='#20a4f3'
+          onClick={(value) => navigate(`/contributions/${value.id}`)}
+        />
+      </BarChart>
+    </SectionContainer>
   );
 };
 
