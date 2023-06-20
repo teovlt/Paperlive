21d9eeb3dca0d0996f005393c33a8317c36e159a
gabrielhalus <gabrielhalus@gmail.com>
2023-06-20 10:35:54 +0200

    🐛 IMPROVE: Team Role Distribution Chart

--- client/src/pages/Statistics/index.jsx
+++ client/src/pages/Statistics/index.jsx
@@ -1,11 +1,12 @@
 import useAuth from '../../hooks/useAuth';
 import { useTranslation } from 'react-i18next';
 import { Heading2 } from '../../theme/appElements';
-import DistributionByRolePerRank from './Charts/DistributionByRolePerRank';
+import AcceptationRejectionChartByType from './Charts/AcceptationRejectionChartByType';
+import TeamRoleDistributionChart from './Charts/TeamRoleDistributionChart';
+
 import ProductionTime from './Charts/ProductionTime';
 import ProductionCost from './Charts/ProductionCost';
 import DistributionPerRank from './Charts/DistributionPerRank';
-import AcceptationRejectionChartByType from './Charts/AcceptationRejectionChartByType';
 
 const Statistics = () => {
   const { t } = useTranslation();
@@ -17,8 +18,8 @@ const Statistics = () => {
     <>
       <Heading2>{t('statistics.statistics')}</Heading2>
       <AcceptationRejectionChartByType contributions={contributions} />
+      <TeamRoleDistributionChart contributions={contributions} />
 
-      {/* <DistributionByRolePerRank contributions={contributions} /> */}
       {/* <ProductionTime contributions={contributions} /> */}
       {/* <ProductionCost contributions={contributions} /> */}
       {/* <DistributionPerRank contributions={contributions} /> */}
