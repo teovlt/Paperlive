52584c1f8f238e91eea7e8eabac291960610016b
gabrielhalus <gabrielhalus@gmail.com>
2023-06-22 16:06:27 +0200

    👌 IMPROVE: Author participation chart

--- client/src/pages/Statistics/Charts/AuthorParticipationCharts.jsx
+++ client/src/pages/Statistics/Charts/AuthorParticipationCharts.jsx
@@ -3,58 +3,80 @@ import { LineChart, Line, CartesianGrid, Tooltip, XAxis, YAxis, Label, Legend } 
 import { Heading3, InlineGroup, SectionContainer } from '../../../theme/appElements';
 import Select from '../../../components/Select';
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router-dom';
 
 const AuthorParticipationCharts = ({ contributions }) => {
   const { t } = useTranslation();
-  const navigate = useNavigate();
 
-  const submissions = contributions.flatMap((c) => c.submissions);
+  const [filter, setFilter] = useState(null);
 
   function getRandomColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
 
-  const filter = {
-    rank: 'B',
-  };
+  const stats = contributions
+    .flatMap((c) => c.submissions.filter((s) => !filter?.type || filter.type === s.venue?.type))
+    .reduce((acc, s) => {
+      const year = new Date(s.submissionDate).getFullYear();
+      if (!year) return acc;
 
-  const stats = Object.entries(
-    submissions
-      .filter(
-        (s) => s.state === 'approved' && s.type === 'longPaper' && s.venue.rank === filter.rank
-      )
-      .reduce((acc, s) => {
-        const year = new Date(s.submissionDate).getFullYear();
+      s.authors?.forEach((author) => {
+        const { _id: id, name } = author;
+        const existingData = acc.find((item) => item.year === year && item.id === id);
 
-        s.authors.flatMap((author) => {
-          const { _id: id } = author;
-          acc[year] = { ...acc[year], [id]: (acc[year]?.[id] ?? 0) + 1 };
-        });
+        if (existingData) existingData.count += 1;
+        else acc.push({ year, id, name, count: 1 });
+      });
 
-        return acc;
-      }, {})
-  )
-    .map(([year, data]) => ({ year, ...data }))
+      return acc;
+    }, [])
     .sort((a, b) => a.year - b.year);
 
   return (
     <SectionContainer>
+      <InlineGroup>
+        <Select
+          label={t('statistics.parameters.venueType')}
+          onChange={(e) => setFilter((filter) => ({ ...filter, type: e.target.value }))}>
+          <option value=''>-</option>
+          <option value='conference'>{t('statistics.parameters.conference')}</option>
+          <option value='journal'>{t('statistics.parameters.journal')}</option>
+        </Select>
+      </InlineGroup>
       <LineChart width={752} height={500} margin={{ top: 15 }} data={stats}>
         <CartesianGrid strokeDasharray='3 3' />
-        <XAxis dataKey='year' type='number' />
-        <YAxis>
-          <Label value='Nombre de participations' angle={-90} position='insideLeft' />
-        </YAxis>
-        <Tooltip /> <Legend />
-        {Object.keys(stats[0]).map((key, index) => {
-          if (key !== 'year') {
-            return (
-              <Line key={index} dataKey={key} stroke={getRandomColor()} />
-            );
+        <Tooltip />
+
+        <XAxis
+          dataKey='year'
+          type='number'
+          domain={[
+            Math.min(...stats.map((item) => item.year)),
+            Math.max(...stats.map((item) => item.year)),
+          ]}
+          tickCount={
+            Math.max(...stats.map((item) => item.year)) -
+            Math.min(...stats.map((item) => item.year))
           }
-          return null;
-        })}
+        />
+
+        <YAxis
+          domain={[0, Math.max(...stats.map((item) => item.count)) + 1]}
+          tickCount={Math.max(...stats.map((item) => item.count)) + 2}
+        />
+
+        {stats.map((authorData) => (
+          <Line
+            key={authorData.id}
+            type='monotone'
+            data={stats.filter(
+              (item) => item.year === authorData.year && item.id === authorData.id
+            )}
+            name={authorData.name}
+            dataKey='count'
+            stroke={getRandomColor()}
+            activeDot={{ r: 8 }}
+          />
+        ))}
       </LineChart>
     </SectionContainer>
   );
