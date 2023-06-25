9fee1d49150e3e6da34c6098e9c7151f027b89a7
gabrielhalus <gabrielhalus@gmail.com>
2023-06-25 10:42:38 +0200

    add: app demo video

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import useAuth from '../../hooks/useAuth';
 import { useTranslation } from 'react-i18next';
 
-import { Heading2, Paragraph } from '../../theme/appElements';
-import { SectionMain } from './homeElements';
+import { Heading2, Paragraph, SectionContainer } from '../../theme/appElements';
 
 const Home = () => {
   const { t } = useTranslation();
@@ -11,14 +10,16 @@ const Home = () => {
 
   return (
     <>
-      <SectionMain>
+      <SectionContainer>
         <Heading2>{t('home.desc')}</Heading2>
         <Paragraph>{auth.description || t('home.noDesc')}</Paragraph>
-      </SectionMain>
-      <SectionMain>
-        <Heading2>{t('home.activity')}</Heading2>
-        <Paragraph>{t('home.noActivity')}</Paragraph>
-      </SectionMain>
+      </SectionContainer>
+      <SectionContainer>
+        <Heading2>{t('home.appDemo')}</Heading2>
+        <video controls>
+          <source src='/videos/demo-paperlive.mp4' type='video/mp4' />
+        </video>
+      </SectionContainer>
     </>
   );
 };
