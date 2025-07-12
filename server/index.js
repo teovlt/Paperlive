e111b170c158f0ce659fbef8efbd4ecd72887ece
teovlt <teo.villet2@gmail.com>
2025-07-12 14:35:45 +0200

    Create uploads folder at the start of the server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,11 @@
 const server = require('./src/server');
 
+import fs from 'fs';
+
+const uploadsPath = './uploads';
+if (!fs.existsSync(uploadsPath)) {
+  fs.mkdirSync(uploadsPath, { recursive: true });
+}
+
 // Start the server
 server;
